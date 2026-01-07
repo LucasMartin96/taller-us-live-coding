971c6b0dc375865a774449a730be778af3022eda
lmartin <lucasmartin766@gmail.com>
2026-01-07 20:28:39 -0300

    add: model transaction

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, Double, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, Double, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 from datetime import datetime
 
 Base = declarative_base()
@@ -12,19 +13,38 @@ class User(Base, TimestampMixin):
     __tablename__ = "User"
     
     id = Column(Integer, autoincrement=True, primary_key=True)
-    name= Column(String, nullable= False)
-    last_name= Column(String, nullable=False)
-    balance =Column(Double, nullable=False, default=0)
-    phone= Column(String, nullable=False)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    balance = Column(Double, nullable=False, default=0)
+    phone = Column(String(10), nullable=False)
     credit_debt = Column(Double, nullable=False, default=0)
+    
+    transactions_sent = relationship('Transaction', foreign_keys='Transaction.payer_id', backref='payer')
+    transactions_received = relationship('Transaction', foreign_keys='Transaction.payee_id', backref='payee')
 
-
+class Transaction(Base, TimestampMixin):
+    __tablename__ = "Transaction"
+    
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    payer_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    payee_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    amount = Column(Double, nullable=False)
+    description = Column(String(100), nullable=True)  
+    balance_used = Column(Double, nullable=False)  
+    credit_used = Column(Double, nullable=False, default=0)  
+    
 
 class Feed(Base, TimestampMixin):
     __tablename__ = "Feed"
-
+    
     id = Column(Integer, autoincrement=True, primary_key=True)
-    user_pay_id = Column(Integer)
-    user_paid_id = Column(Integer)
-    ammount = Column(Double, nullable=False)
-    feed_type = Column(String, nullable=False)
+    feed_type = Column(String(15), nullable=False)  # "payment" or "friend_add"
+    
+    transaction_id = Column(Integer, ForeignKey('Transaction.id'), nullable=True)
+    
+    user_pay_id = Column(Integer, ForeignKey('User.id'), nullable=True)
+    user_paid_id = Column(Integer, ForeignKey('User.id'), nullable=True)
+    
+    transaction = relationship('Transaction', backref='feed_entries')
+    user_pay = relationship('User', foreign_keys=[user_pay_id])
+    user_paid = relationship('User', foreign_keys=[user_paid_id])
