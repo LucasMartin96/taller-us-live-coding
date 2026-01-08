18c32461c09dd280688cfafe03233f7749d90507
lmartin <lucasmartin766@gmail.com>
2026-01-07 23:21:36 -0300

    import in the module

---
+++ services/__init__.py
@@ -0,0 +1,5 @@
+from feed_service import FeedService
+from friend_service import FriendService
+from payment_service import PaymentService
+from transaction_service import TransactionService
+from user_service import UserService
