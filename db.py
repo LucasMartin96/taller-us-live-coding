593dd2c57a6a532db00f11fba89ee5b9426b29ea
lmartin <lucasmartin766@gmail.com>
2026-01-07 21:28:52 -0300

    fix db

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///db.sql")
+engine = create_engine("sqlite:///db.db")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
