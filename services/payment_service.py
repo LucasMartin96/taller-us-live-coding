788a4b3ce6a52c41ce9b26013310eb5d44acff9b
lmartin <lucasmartin766@gmail.com>
2026-01-07 21:42:46 -0300

    add: execute payment function plus fixed typo in ammount -> amount

--- services/payment_service.py
+++ services/payment_service.py
@@ -3,10 +3,20 @@ from models import User
 
 
 class PaymentService():
-    def calculate_payments_credit_balance(self, payer : User, ammount: float):
-        balance_use = min(payer.balance, ammount)
-        credit_use = max(0.0, ammount - payer.balance)
+    def calculate_payments_credit_balance(self, payer : User, amount: float):
+        balance_use = min(payer.balance, amount)
+        credit_use = max(0.0, amount - payer.balance)
         return balance_use, credit_use
     
+    def execute_payment(self, payer: User, payee, amount: float):
+        payee.balance += amount
+        
+        if payer.balance >= amount:
+            payer.balance -= amount
+            return
+        
+        credit_card_amount = amount - payer.balance
+        payer.balance = 0
+        payer.credit_debt += credit_card_amount # I assume this is credit card debt without limits
 
     
