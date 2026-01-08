8cdf8eda0371e88672e83b028d18059c365058fe
lmartin <lucasmartin766@gmail.com>
2026-01-07 23:21:12 -0300

    dependency configuration

--- dependencies.py
+++ dependencies.py
@@ -1,4 +1,8 @@
 from db import SessionLocal
+from services import UserService, TransactionService, FeedService, FriendService
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
 
 def get_db():
     db = SessionLocal()
@@ -6,4 +10,15 @@ def get_db():
         yield db
     finally:
         db.close()
-        
+    
+def get_user_service(db: Session = Depends(get_db)) -> UserService:
+    return UserService(db)
+
+def get_transaction_service(db: Session = Depends(get_db)) -> TransactionService:
+    return TransactionService(db)
+
+def get_feed_service(db: Session = Depends(get_db)) -> FeedService:
+    return FeedService(db)
+
+def get_friend_service(db: Session = Depends(get_db)) -> FriendService:
+    return FriendService(db)
