afc84a038a643825d1f3dc03daba83e5a79aaa30
lmartin <lucasmartin766@gmail.com>
2026-01-07 21:41:04 -0300

    quick refactor payer is better than user to pay

--- services/payment_service.py
+++ services/payment_service.py
@@ -3,9 +3,10 @@ from models import User
 
 
 class PaymentService():
-    def calculate_payments_credit_balance(self, user_to_pay: User, ammount: float):
-        balance_use = min(user_to_pay.balance, ammount)
-        credit_use = max(0.0, ammount - user_to_pay.balance)
+    def calculate_payments_credit_balance(self, payer : User, ammount: float):
+        balance_use = min(payer.balance, ammount)
+        credit_use = max(0.0, ammount - payer.balance)
         return balance_use, credit_use
     
+
     
