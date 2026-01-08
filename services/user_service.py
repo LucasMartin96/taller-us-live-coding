7605693d3aa533a5bbe3639fc0405d444d306228
lmartin <lucasmartin766@gmail.com>
2026-01-07 21:47:57 -0300

    add refresh so it has the id, add try except for rollback in case of error.

--- services/user_service.py
+++ services/user_service.py
@@ -7,12 +7,29 @@ class UserService:
         self.db = db
     
     def create_user(self, req: CreateUserRequest):
-
-        user = User(name=req.name, last_name=req.last_name, phone=req.phone,balance=req.balance,credit_debt=req.credit_debt)
-        self.db.add(user)
-        self.db.commit()
+        try:
+            user = User(
+                name=req.name,
+                last_name=req.last_name,
+                phone=req.phone,
+                balance=req.balance,
+                credit_debt=req.credit_debt
+            )
+            self.db.add(user)
+            self.db.commit()
+            self.db.refresh(user)
             
-        return CreatedUserResponse(id=user.id, name=user.name, last_name=user.last_name, phone=user.phone, balance=user.balance, credit_debt=user.credit_debt )
+            return CreatedUserResponse(
+                id=user.id,
+                name=user.name,
+                last_name=user.last_name,
+                phone=user.phone,
+                balance=user.balance,
+                credit_debt=user.credit_debt
+            )
+        except Exception as e:
+            self.db.rollback()
+            raise
     
     def get_user(self, user_id: int) -> User:
 
