a7f1ffa7251aaa8a4ee6a1ea2175bb99190b344b
lmartin <lucasmartin766@gmail.com>
2026-01-07 23:11:25 -0300

    transaction service implementation

--- services/transaction_service.py
+++ services/transaction_service.py
@@ -1,7 +1,41 @@
 from sqlalchemy.orm import Session
 from models import Transaction, User
 from services.payment_service import PaymentService
-from services.feed_service import FeedService
 
 
+class TransactionService:
+    def __init__(self, db: Session, payment_service: PaymentService = None):
+        self.db = db
+        self.payment_service = payment_service or PaymentService()
+    
+    def create_payment_transaction(
+        self, 
+        payer: User, 
+        payee: User, 
+        amount: float,
+        description: str = ""
+    ):
+        try:
+            balance_used, credit_used = self.payment_service.calculate_payment_split(payer, amount)
+            
+            self.payment_service.execute_payment(payer, payee, amount)
+            
+            transaction = Transaction(
+                payer_id=payer.id,
+                payee_id=payee.id,
+                amount=amount,
+                description=description,
+                balance_used=balance_used,
+                credit_used=credit_used
+            )
+            self.db.add(transaction)
+            self.db.flush() 
+            
+            # TODO: Create payment feed HERE. Create services for it
+            
+            self.db.commit()
+            return transaction
+        except Exception as e:
+            self.db.rollback()
+            raise
 
